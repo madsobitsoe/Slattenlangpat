5a237f5f09e76bd311237d94983936f5e2689291
Mads Obitsø <madsobitsoe@gmail.com>
2020-12-15 16:38:01 +0100

    Add environments and vars to interpreter

--- src/interpreter.fs
+++ src/interpreter.fs
@@ -5,37 +5,66 @@ type EvalError = string
 type EvalResult = int
 type Eval = Result<EvalResult,EvalError>
 
+
+// infix for Result.bind, so it looks like I know what I'm doing.
 let ( >>= ) r f = r |> Result.bind f
 
 let a : Result<int,string> = Ok 1
 a >>= (fun x -> Ok x)
 
 
+// An environment is a function
+type Environment = VName -> Result<Expr,string>
+// The empty environment always returns an error
+let initEnv:Environment = (fun name -> Error <| sprintf "Variable %s not found in environment" name)
+// Extending environments means wrapping the previous environment in a new environment
+let extendEnv name expr env = (fun x -> if x = name then Ok expr else env x)
+// Only needed when piping. An environment is a function, so lookup is `flip`
+let lookup name env = env name
+
+// Usage of environments
+// initEnv "x"
+// |> extendEnv "x" 1
+// |> lookup "x" // result is Ok 1
+
+// initEnv
+// |> extendEnv "x" 1
+// |> lookup "y" // result is Error "Variable y not found in environment"
 
 
-let rec eval:(Expr->Eval) = function
-    | Const i -> Ok i
-    | Add (e1,e2) ->
-        eval e1 >>=
+// This should evaluate `173034579` and print "SLP\n\x00\x00\x00\x00"
+// eval (Print (Const 173034579))
+
+
+
+let rec eval':(Expr * Environment -> Eval) = function
+    | ((Const i),env) -> Ok i
+    | Add (e1,e2), env ->
+        eval' (e1,env) >>=
             (fun e1res ->
-                     eval e2 >>= (fun e2res -> e1res + e2res |> Ok))
+                     eval' (e2,env) >>= (fun e2res -> e1res + e2res |> Ok))
 
-    | Sub (e1,e2) ->
-        eval e1 >>=
+    | Sub (e1,e2),env ->
+        eval' (e1,env) >>=
             (fun e1res ->
-                     eval e2 >>= (fun e2res -> e1res - e2res |> Ok))
+                     eval' (e2,env) >>= (fun e2res -> e1res - e2res |> Ok))
 
-    // Currently, evaluates to int and prints the first 4 bytes as their ascii-value
-    // This is STUPID AF, but it is what the code generator does.
-    // The interpreter should always mimic the code generator, not the other way around
-            // A print doesn't "evaluate" to anything
+            // Currently, evaluates to int and prints the first 4 bytes as their ascii-value
+            // This is STUPID AF, but it is what the code generator does.
+            // The interpreter should always mimic the code generator, not the other way around
+            // A print in SLP doesn't "evaluate" to anything
             // The codegenerator will currently
-            // generate instruction for the expression, store the result in RAX (hopefully), push it to stack and call SYS_WRITE with a pointer to the stack.
+            // generate instruction for the expression, store the result in RAX (hopefully), push it to stack and call SYS_WRITE with a pointer to the stack and write 8 bytes. At least four of these will be NULL-bytes.
             // NOTE: In the interpreter, a Print evaluates to the value of the expression being printed.
-    | Print e ->
-        eval e >>= (fun x ->
-                    // Convert the int32 to 4 bytes
-                    System.BitConverter.GetBytes (x)
+            // F# requires all branches in a function to evaluate to the same type.
+            // Since we don't have any types in SLP (yet), this will have to do.
+            // The Expr below should evaluate `173034579` and print "SLP\n\x00\x00\x00\x00"
+            // eval (Print (Const 173034579))
+
+    | Print e,env ->
+        eval' (e,env) >>= (fun x ->
+                    // Convert the int32 to 4 bytes and append the 4 null-bytes
+                    Array.append  (System.BitConverter.GetBytes (x)) (System.BitConverter.GetBytes(0))
                     |> Array.map char
                     |> Array.map string
                     |> Array.reduce (+)
@@ -43,3 +72,9 @@ let rec eval:(Expr->Eval) = function
                     // (x >>> 24) &&& 0xff = 0xa
                     // Therefore printf completely by intention
                     |> (fun s -> printf "%s" s; Ok x))
+        // Expressions are evaluated before they are stored in an environment.
+        // We should definitely have a value type
+    | Var (name),env -> env name >>= (fun res -> eval' (res,env))
+    | unimplemented -> sprintf "The expression '%A' is sadly not implemented yet. \nCheck https://github.com/madsobitsoe/Slattenlangpat/issues and maybe add an issue or submit a pull request." (fst unimplemented) |> Error
+// The "public" api.
+let eval = function | x -> eval' (x,initEnv)
