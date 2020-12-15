de832c4568520c00ff729460e5512226671b751f
Mads Obitsø <madsobitsoe@gmail.com>
2020-12-15 16:27:22 +0100

    Add local vars to ast

--- src/ast.fs
+++ src/ast.fs
@@ -1,9 +1,13 @@
 module AST
 
+
+type VName = string
 type Expr =
     | Const of int
     | Add of (Expr * Expr)
     | Sub of (Expr * Expr)
+    | Var of VName
+    | Let of VName * Expr * Expr
     | Print of Expr
 
 type Program = Expr list
