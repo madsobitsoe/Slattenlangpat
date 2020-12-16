59efcf13cdff1aa9f41893b8a5c120c3ecde3e53
Mads Obitsø <madsobitsoe@gmail.com>
2020-12-16 14:14:42 +0100

    Add debug-mode to REPL

--- src/main.fs
+++ src/main.fs
@@ -22,32 +22,54 @@ let rec repl handler =
     match System.Console.ReadLine() with
     | "exit" | "q" | "quit" -> ()
     | input ->
-        parse input
-        |> Result.bind eval
-        |> handler; repl handler
+        handler input; repl handler
+        // parse input
+        // |> Result.bind eval
+        // |> handler; repl handler
 
-let repl' () =
+let pRes = function
+    | Ok res -> printfn "%d" res
+    | Error msg -> printfn "%s" msg
+
+let rec default'handler = (parse >> Result.bind eval >> pRes)
+let rec debug'handler input =
+    printfn "Parsing %A" input
+    let parseRes = parse input
+    printfn "Result of parse: %A" parseRes
+    printfn "Evaluating in the interpreter..."
+    let eRes = Result.bind eval parseRes
+    printf "Result of eval: "
+    pRes eRes
+
+
+
+
+
+let repl' debug =
     let replmsg =
           "Starting SLP interactive mode..."
-        + "type exit, quit or q to quit"
+        + "\nType exit, quit or q to quit."
     printfn "%s" replmsg
-    let handler res =
-        match res with
-            | Ok res -> printfn "%d" res
-            | Error msg -> printfn "%s" msg
-    repl handler
+    // let handler res =
+    //     match res with
+    //         | Ok res -> printfn "%d" res
+    //         | Error msg -> printfn "%s" msg
+    if debug then repl debug'handler
+    else repl default'handler
     0
 
 
 let usage =
-      "Usage: \n\tslpc input.slp [-o executableName]"
-    + "\tInteractive mode: slpc -i"
+      "Usage: \n\tslpc input.slp [-o executableName]\n"
+    + "\tInteractive mode: slpc -i [-d]\n"
+
 
 [<EntryPoint>]
 let main args =
     let exitcode =
         match args with
-        | [|"-i"|] ->  repl' ()
+        | [|"-i"|] ->  repl' false
+        | [|"-i";"-d"|] ->  repl' true
         | [|inputFilePath;"-o";outputFilePath|] ->
 
             printfn "Will compile %s and save as %s" inputFilePath outputFilePath
