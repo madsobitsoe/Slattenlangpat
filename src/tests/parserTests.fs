010340f917bb87be279ffd0e03a70e23e5142e80
Mads Obitsø <madsobitsoe@gmail.com>
2020-12-21 09:32:55 +0100

    Rewrite parsertests

--- src/tests/parserTests.fs
+++ src/tests/parserTests.fs
@@ -3,41 +3,41 @@ open AST
 open Parser
 open TestUtil
 
-let testcases'const : TestCase<string,Result<Expr,string>> list =
+let testcases'const : TestCase<string,Result<Program,string>> list =
     [
-        "1",  Ok (Const (Int 1));
-        "2",  Ok (Const (Int 2));
-        "3",  Ok (Const (Int 3));
-        "0",  Ok (Const (Int 0));
-        "-1", Ok (Const (Int (-1)));
-        "01", Ok (Const (Int 1));
-        "10", Ok (Const (Int 10));
-        "123456789",Ok (Const (Int 123456789));
+        "1",  Ok       [SExp (Const (Int 1))];
+        "2",  Ok       [SExp (Const (Int 2))];
+        "3",  Ok       [SExp (Const (Int 3))];
+        "0",  Ok       [SExp (Const (Int 0))];
+        "-1", Ok       [SExp (Const (Int (-1)))];
+        "01", Ok       [SExp (Const (Int 1))];
+        "10", Ok       [SExp (Const (Int 10))];
+        "123456789",Ok [SExp (Const (Int 123456789))];
         ] |> List.map returnT
 
-let testcases'add : TestCase<string,Result<Expr,string>> list =
+let testcases'add : TestCase<string,Result<Program,string>> list =
     [
-        "0+0", Ok (Add (Const (Int 0), Const (Int 0)));
-        "1+1", Ok (Add (Const (Int 1), Const (Int 1)));
-        "2+2", Ok (Add (Const (Int 2), Const (Int 2)));
-        "3+3", Ok (Add (Const (Int 3), Const (Int 3)));
-        "4+4", Ok (Add (Const (Int 4), Const (Int 4)));
-        "1+2+3",Ok (Add (Add (Const (Int 1), Const (Int 2)), Const (Int 3))); // left associativity
-        "1+2+3+4",Ok (Add (Add (Add (Const (Int 1), Const (Int 2)), Const (Int 3)), Const (Int 4))); // left associativity
+        "0+0",    Ok [SExp (Oper (Plus, (Const (Int 0)), Const (Int 0)))];
+        "1+1",    Ok [SExp (Oper (Plus, (Const (Int 1)), Const (Int 1)))];
+        "2+2",    Ok [SExp (Oper (Plus, (Const (Int 2)), Const (Int 2)))];
+        "3+3",    Ok [SExp (Oper (Plus, (Const (Int 3)), Const (Int 3)))];
+        "4+4",    Ok [SExp (Oper (Plus, (Const (Int 4)), Const (Int 4)))];
+        "1+2+3",  Ok [SExp (Oper (Plus, (Oper (Plus, Const (Int 1), Const (Int 2))), Const (Int 3)))]; // left associativity
+        "1+2+3+4",Ok [SExp (Oper (Plus, Oper (Plus, Oper (Plus, Const (Int 1), Const (Int 2)), Const (Int 3)), Const (Int 4)))]; // left associativi)ty
         ] |> List.map returnT
 
-let testcases'add'parens : TestCase<string,Result<Expr,string>> list =
+let testcases'add'parens : TestCase<string,Result<Program,string>> list =
     [
 
-        "(1+2)+3", Ok (Add (Add (Const (Int 1), Const (Int 2)), Const (Int 3)));
-        "(1 + 2) + 3", Ok (Add (Add (Const (Int 1), Const (Int 2)), Const (Int 3)));
-        "(1 + 2) + 3 ", Ok (Add (Add (Const (Int 1), Const (Int 2)), Const (Int 3)));
-        "((1 + 2) + 3)", Ok (Add (Add (Const (Int 1), Const (Int 2)), Const (Int 3)));
-        "1+(2+3)", Ok (Add (Const (Int 1), (Add (Const (Int 2), Const (Int 3)))));
-        "1+(2+3+4)", Ok (Add (Const (Int 1), (Add (Add (Const (Int 2), Const (Int 3)), Const (Int 4)))));
+        "(1+2)+3"       , Ok [SExp (Oper (Plus, Oper (Plus, Const (Int 1), Const (Int 2)), Const (Int 3)))];
+        "(1 + 2) + 3"   , Ok [SExp (Oper (Plus, Oper (Plus, Const (Int 1), Const (Int 2)), Const (Int 3)))];
+        "(1 + 2) + 3 "  , Ok [SExp (Oper (Plus, Oper (Plus, Const (Int 1), Const (Int 2)), Const (Int 3)))];
+        "((1 + 2) + 3)" , Ok [SExp (Oper (Plus, Oper (Plus, Const (Int 1), Const (Int 2)), Const (Int 3)))];
+        "1+(2+3)"       , Ok [SExp (Oper (Plus, Const (Int 1), Oper (Plus, Const (Int 2), Const (Int 3))))];
+        "1+(2+3+4)"     , Ok [SExp (Oper (Plus, Const (Int 1), Oper (Plus, Oper (Plus, Const (Int 2), Const (Int 3)), Const (Int 4))))];
         ] |> List.map returnT
 
-let testcases'invalid'parses : TestCase<string,Result<Expr,string>> list =
+let testcases'invalid'parses : TestCase<string,Result<Program,string>> list =
     [
 
         "(1", Error "";
@@ -57,7 +57,7 @@ let parserTests = ATestSuite <| (test parse,
                                  @ testcases'invalid'parses)
 
 let testResults = runTests parserTests
-let passed : TestResult<string,Result<Expr,string>> list =
+let passed : TestResult<string,Result<Program,string>> list =
     testResults |> List.choose (function | Success tc -> Some (Success tc) | _ -> None)
 
 let failed = testResults |> List.choose (function | Failure (tc,got) -> Some (Failure (tc,got)) | _ -> None)
